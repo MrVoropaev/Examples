d0a46a2dde2aca1de45f3bef0caa6663be3bf5fc
MrVoropaev <mrvoropaev@gmail.com>
2022-11-30 19:49:32 +0300

    Example010

---
+++ Example010/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = {1, 21, 32, 43, 54, 65, 65, 65, 55, 42, 31, 11};
+
+int n = array.Length;
+int find = 65;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index = index + 1;
+}
